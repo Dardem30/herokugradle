7cbabaca10740ee2a5d902c5bec54ffdadba36b2
dardem30 <r.lukashenko@mail.ru>
2017-10-30 23:36:28 +0300

    Initial commit

---
+++ src/main/java/com/example/demo/GradleherokuApplication.java
@@ -0,0 +1,12 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GradleherokuApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GradleherokuApplication.class, args);
+	}
+}
